7f0e914b4e4be57779f46cdb31ef961477d491a2
Sam Paniagua <73447315+sampaniagua@users.noreply.github.com>
2023-05-11 16:56:12 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use(cors());
+
+// This is your proxy middleware configuration.
+app.use('/sdapi', createProxyMiddleware({
+  target: 'https://pyy7zm1qnwzcs1-3000.proxy.runpod.net',
+  changeOrigin: true,
+}));
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server with proxy listening on port 3000');
+});
+
